a39ba2c6b06bc9a632fb34d50fd9e8e1c3bc2fde
SergSmilya <sergsmilichenko@gmail.com>
2022-12-22 19:43:02 +0200

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -13,24 +13,51 @@ refs.divEl.addEventListener("input", onInputValue);
 refs.createButtonEl.addEventListener("click", onCreatElement);
 refs.destroyButtonEl.addEventListener("click", onDestroyElement);
 
+let amountValue = 0;
+
+const divElements = [];
+
 function onInputValue(e) {
-  return (amount = +e.target.value);
-}
-function onCreatElement() {
-  createBoxes();
+  amountValue = e.target.valueAsNumber;
 }
 
-function onDestroyElement(e) {
+function onCreatElement() {
+  createBoxes(amountValue);
+}
+
+function onDestroyElement() {
   destroyBoxes();
 }
 
-function createBoxes() {
-  refs.newElinDivBoxes.insertAdjacentHTML(
-    "afterend",
-    `<div backgroundColor=${getRandomHexColor}>jhjhjh</div>`
-  );
-}
+function createBoxes(amountValue) {
+  // for (let i = 1; i <= amountValue; i += 1) {
+  //   const el = document.createElement("div");
+  //   el.style.backgroundColor = getRandomHexColor();
+  //   el.style.width = 300;
+  //   el.style.height = 200;
 
+  //   divElements.push(el);
+  // }
+
+  // console.log(...divElements);
+  // refs.newElinDivBoxes.append(...divElements);
+
+  // for (let i = 0; i < amountValue; i += 1) {
+  //   const el = `<div style="background-color:${getRandomHexColor()}; width: 30px; height: 30px;"></div>`;
+
+  //   divElements.push(el);
+  // }
+
+  // refs.newElinDivBoxes.insertAdjacentHTML("beforeend", divElements.join(""));
+
+  for (let i = 0; i < amountValue; i += 1) {
+    refs.newElinDivBoxes.insertAdjacentHTML(
+      "beforeend",
+      `<div style="background-color:${getRandomHexColor()}; width: 30px; height: 30px;"></div>`
+    );
+  }
+}
 function destroyBoxes() {
-  refs.newElinDivBoxes.remove();
+  refs.newElinDivBoxes.innerHTML = "";
+  refs.divEl.firstElementChild.value = "";
 }
